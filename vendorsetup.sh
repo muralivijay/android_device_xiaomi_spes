1ceb054af5d10d14a06f2338e343bb73812e8e99
muralivijay <muralidharan9845@gmail.com>
2024-12-26 00:17:51 +0530

    spes: Setup kernel/vendor/xiaomi-hardware tree on vendorsetup.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,9 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+# Clone kernel/vendortree/xiaomi-hardware
+echo -e "${color}Setuo kernel,vendor,xiaomi-hardware tree ${end}"
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git -b main kernel/xiaomi/sm6225
+git clone https://github.com/muralivijay/android_vendor_xiaomi_spes.git -b 13.0 vendor/xiaomi/spes
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xioami/
