556be271a2065a3eeecfe3a1d9761a68ef5eabee
muralivijay <muralidharan9845@gmail.com>
2024-12-26 00:37:34 +0530

    spes: setup miuicamera

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,7 +13,11 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 rm -rf hardware/lineage/compat/Android.bp
 
 # Clone kernel/vendortree/xiaomi-hardware
-echo -e "${color}Setuo kernel,vendor,xiaomi-hardware tree ${end}"
+echo -e "${color}Setup kernel,vendor,xiaomi-hardware tree ${end}"
 git clone https://github.com/muralivijay/kernel_xiaomi_sm6225.git -b main kernel/xiaomi/sm6225
 git clone https://github.com/muralivijay/android_vendor_xiaomi_spes.git -b 13.0 vendor/xiaomi/spes
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xioami/
+
+# Clone Miuicamera
+echo -e "${color}Setup Miuicamera ${end}"
+git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm6225 vendor/xiaomi/camera/
