63d5cb7e4146bf875a092effbd1ef2d16dfdd666
muralivijay <muralidharan9845@gmail.com>
2025-07-10 23:49:28 +0530

    Revert "spes: Use spesific GApps for spes"
    This reverts commit 1076e3623225c4b5386d87823dc18a8c046c4ce0.

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -15,10 +15,8 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Additions
+$(call inherit-product-if-exists, vendor/gapps/common.mk)
 $(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
-
-# Inherit GApps if available
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 
 # Product Specifics
 PRODUCT_NAME := lineage_spes
