e6796d4cc68555fbe2868bc5615d822a2944fdc8
RAM-UNLOK <rakmoparte@gmail.com>
2025-09-20 02:05:00 +0530

    spes: Add undeclared android.hardware.graphics.composer@2.1 HAL in device manifest
    Unable to set property "ctl.interface_start" to "android.hardware.graphics.composer@2.1::IComposer/default": error code: 0x20
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -181,6 +181,7 @@ SYSTEMUI_OPTIMIZE_JAVA := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    android.hardware.graphics.composer@2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     memtrack.bengal
