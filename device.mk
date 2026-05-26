add7e1d1bee40a2eed1d91d92e696c554ae796e9
muralivijay <muralidharan9845@gmail.com>
2026-05-26 16:00:51 +0000

    spes: Disable Blur support.
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# BLUR
+TARGET_SUPPORTS_BLUR := false
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1 \
