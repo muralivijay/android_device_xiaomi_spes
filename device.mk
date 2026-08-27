0ad2e75d79297eb48a9fadd9aab8460fa931c6e8
LuK1337 <priv.luk@gmail.com>
2026-08-27 19:48:32 +0530

    spes: Opt out of speaker_layout_channel_mask field
    * Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # BLUR
 TARGET_SUPPORTS_BLUR := false
 
